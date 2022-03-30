ee6048702453df6aaf794afa4c85d5ee217b6baf
nikhilgharge4591 <nikhilgharge93@gmail.com>
2022-03-30 12:06:41 +0100

    Initial Commit

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Nikhil Gharge on 30/03/22.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description:String?
+    let location:String?
+    let imageURL:URL?
+}
