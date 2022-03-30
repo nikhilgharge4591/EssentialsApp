56b08cc80e0e0f7303a76740b42ded4a3a9ed57c
nikhilgharge4591 <nikhilgharge93@gmail.com>
2022-03-30 13:10:50 +0100

    Move Separate Logic to HTTPClientSpy

--- EssentialFeedTests/RemoteFeedLoaderClass.swift
+++ EssentialFeedTests/RemoteFeedLoaderClass.swift
@@ -9,29 +9,38 @@ import XCTest
 
 class RemoteFeedLoader{
     func load(){
-        HTTPClient.shared.requestedURL = URL(string:"https://www.algoexpert.io/data-structures")
+        HTTPClient.shared.getURL(from:URL(string:"https://www.algoexpert.io/data-structures")!)
     }
 }
 
 class HTTPClient{
     
-    static let shared = HTTPClient()
+    static var shared = HTTPClient()
     
-    private init(){}
+    func getURL(from url: URL) {}
+    
+}
+
+class HTTPClientSpy: HTTPClient{
+    
+    override func getURL(from url: URL){
+        requestedURL = url
+    }
     
     var requestedURL: URL?
+    
 }
 
 class RemoteFeedLoaderClass: XCTestCase {
 
     func test_init_testDoesNotRequestDataFromURL(){
         _ = RemoteFeedLoader()
-        let client = HTTPClient.shared
+        let client = HTTPClientSpy()
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_requestDataFromURL(){
-        let client = HTTPClient.shared
+        let client = HTTPClientSpy()
         let sut = RemoteFeedLoader()
         
         sut.load()
